0e623abefba8ba5ec2564b5c84af2af935da634b
ginsengkid <gvv55221133@gmail.com>
2022-01-13 12:16:33 +0300

    tree addedd

--- src/tree/TestTree.java
+++ src/tree/TestTree.java
@@ -2,19 +2,13 @@ package tree;
 
 public class TestTree {
     public static void main(String[] args) {
-        Tree t = new Tree();
-        t.create('a');
-        Tree.printMem();
-        System.out.println();
-
         Tree t1 = new Tree();
-        t1.create('b');
-        Tree.printMem();
-        System.out.println();
+        t1.create('a');
+
+        Tree t = new Tree();
+        t.create('b');
 
         t1.create('c', t);
-        Tree.printMem();
-        System.out.println();
 
         t = new Tree();
         t.create('d');
@@ -26,11 +20,39 @@ public class TestTree {
 
         t1.create('G', t);
         Tree.printMem();
+        System.out.println();
+        System.out.println();
+        System.out.println();
 
+        int l1 = t1.leftMostChild(t1.root());
+        System.out.println(t1.label(l1));
+        System.out.println(t1.label(t1.rightSibling(l1)));
+        System.out.println(t1.label(t1.parent(l1)));
+        System.out.println(t1.label(t1.parent(t1.rightSibling(l1))));
+        System.out.println();
 
-        System.out.println(t1.root());
-        int l = t1.leftMostChild(t1.leftMostChild(t1.root()));
+        int l = t1.leftMostChild(l1);
         System.out.println(t1.label(l));
-    }
+        System.out.println(t1.label(t1.rightSibling(l)));
+        System.out.println(t1.label(t1.parent(l)));
+        System.out.println(t1.label(t1.parent(t1.rightSibling(l))));
+        System.out.println();
 
+        int l2 = t1.rightSibling(l1);
+        System.out.println(t1.label(t1.leftMostChild(l2)));
+        System.out.println(t1.label(t1.rightSibling(t1.leftMostChild(l2))));
+        System.out.println(t1.label(t1.parent(t1.leftMostChild(l2))));
+        System.out.println(t1.label(t1.parent(t1.rightSibling(t1.leftMostChild(l2)))));
+        System.out.println();
+
+
+
+
+       /* Tree t3 = new Tree();
+        t3.create('q');
+        l = t3.root();
+        System.out.println(t1.label(l));
+        System.out.println();*/
+
+    }
 }
--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -10,8 +10,8 @@ public class Tree {
         public ArrayNode(char l, Node n1, int n2){
             label = l;
             next = n1;
-
         }
+
         public ArrayNode(int n){
             name = n;
         }
@@ -30,7 +30,7 @@ public class Tree {
     private int root;
     private static ArrayNode[] array;
     private static int space;
-    private final static int len = 50;
+    private final static int len = 10;
 
     static {
         array = new ArrayNode[len];
@@ -38,7 +38,7 @@ public class Tree {
             array[i] = new ArrayNode(i+1);
         }
         space = 0;
-        array[49] = new ArrayNode(-1);
+        array[len - 1] = new ArrayNode(-1);
     }
 
     public Tree(){
@@ -47,32 +47,27 @@ public class Tree {
 
     public Tree create(char l){
         if (space == -1) return null;
-        if (root == -1) {
-            root = space;
-            space = array[space].name;
-            array[root].label = l;
+        if (root != -1) {
+            array[space].next = new Node(root, null);
         }
-        else {
-            int temp = space;
-            space = array[space].name;
-            array[temp].label = l;
-            array[temp].next = new Node(root, null);
-            root = temp;
-        }
+        array[space].label = l;
+        root = space;
+        space = array[space].name;
+
         return this;
     }
 
     public Tree create(char l, Tree t){
-        if (space == -1) return null;
+        if (space == -1 || t == null) return null;
 
         if (root == -1) return t.create(l);
         if (t.root == -1) return create(l);
 
-        int temp = space;
+        array[space].label = l;
+        array[space].next = new Node(root, new Node(t.root, null));
+        root = space;
         space = array[space].name;
-        array[temp].label = l;
-        array[temp].next = new Node(root, new Node(t.root, null));
-        root = temp;
+
         return this;
     }
 
@@ -82,19 +77,39 @@ public class Tree {
 
     public int leftMostChild(int n){
         if (array[n].next == null) return -1;
-        if (array[n].next.next == null) return -1;
+
         if (n == root && array[n].next != null)
             return array[n].next.name;
+
         int temp = ifInTree(n, root);
         if (temp == -1) return -1;
+
         else return array[n].next.name;
     }
 
     public char label(int n){
+        if (n == root) return array[n].label;
         int a = ifInTree(n, root);
-        System.out.println(a);
-        if (a != -1) return array[n].label;
-        else return ' ';
+        if (a != -1) {
+            return array[n].label;
+        }
+        else return '-';
+    }
+
+    public int rightSibling(int n){
+        if (n == root) return -1;
+
+        int temp = ifInTree(n, root);
+        if (temp == -1) return -1;
+        if (array[temp].next.next != null) return array[temp].next.next.name;
+
+        return -1;
+
+    }
+
+    public int parent(int n){
+        if (n == -1 || n == root || n > len) return -1;
+        return ifInTree(n, root);
     }
 
     public void print(){
@@ -111,29 +126,29 @@ public class Tree {
                     System.out.print(" " + array[i].next.next.name);
                 }
             }
-
             System.out.println();
         }
     }
 
+    //return parent
     private int ifInTree(int n, int r){
         Node left = array[r].next;
         if (left == null) return -1;
 
         if (n == left.name) return r;
 
-
         Node right = left.next;
         if (right != null) {
             if (right.name == n)
                 return r;
         }
-        else return -1;
 
+        int temp = ifInTree(n, left.name);
 
-        ifInTree(n, left.name);
-        ifInTree(n, right.name);
-        return -1;
+        if (temp == - 1 && right != null) {
+            return ifInTree(n, right.name);
+        }
+        return temp;
     }
 
 }
